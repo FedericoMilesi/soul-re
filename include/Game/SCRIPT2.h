ab2aa3297ab803037c9da807cb6bfea768bcdbda
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-05-14 17:15:00 -0300

    Matched LitShaftInit

---
+++ include/Game/SCRIPT2.h
@@ -0,0 +1,7 @@
+// size: 0x4
+struct LitShaftProperties {
+	// offset: 0000
+	short fadeValue;
+	// offset: 0002
+	short pad;
+};
--- src/Game/SCRIPT2.c
+++ src/Game/SCRIPT2.c
@@ -1,3 +1,29 @@
 #include "common.h"
+#include "Game/SCRIPT2.h"
+#include "Game/INSTANCE.h"
 
-INCLUDE_ASM("asm/nonmatchings/Game/SCRIPT2", LitShaftInit);
+void LitShaftInit(struct _Instance *instance, struct GameTracker *gameTracker)
+{
+    struct LitShaftProperties *Prop; 
+    
+    Prop = (struct LitShaftProperties*)instance->data;
+    
+    if (Prop != NULL) 
+    {
+        int curTimeOfDay;
+        
+        curTimeOfDay = GAMELOOP_GetTimeOfDay();
+        
+        switch (curTimeOfDay) 
+        { 
+        case 1900:
+        case 600: 
+            instance->fadeValue = 4096;
+            break;
+        case 700: 
+        case 1800:
+            instance->fadeValue = Prop->fadeValue;
+            break;
+        }
+    }
+}
