6442ee9bd2172ab83b8a6c1e23a4915715495a7a
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-05-14 19:55:01 -0300

    Match RELMOD_InitModulePointers

--- src/Game/RELMOD.c
+++ src/Game/RELMOD.c
@@ -1,5 +1,36 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/Game/RELMOD", RELMOD_InitModulePointers);
+void RELMOD_InitModulePointers(int baseaddr, int *relocs)
+{ 
+	unsigned int* rel_addr;
+    int temp; // not from decls.h
+
+    while (*relocs != -1) 
+    {
+        rel_addr = (unsigned int*)(baseaddr + ((*relocs & ~0x3)));
+
+        switch ((*relocs++ & 0x3))
+        {
+        case 0:
+            temp = *rel_addr;
+            
+            if (temp >= 0)
+            {
+                *rel_addr += baseaddr;
+            }
+            
+            break;
+        case 1:
+            *(short*)rel_addr = ((baseaddr + *relocs++) + 32768) >> 16; 
+            break;
+        case 2:
+            *(short*)rel_addr += baseaddr;
+            break;
+        case 3:
+            *rel_addr += ((unsigned int)baseaddr << 4) >> 6;
+            break;
+        }
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/RELMOD", RELMOD_RelocModulePointers);
