6b6261d3c0db65142576969212c49911179e2ac6
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-05-14 17:21:53 -0300

    Matched MATH3D_FastAtan2

--- src/Game/MATH3D.c
+++ src/Game/MATH3D.c
@@ -55,7 +55,71 @@ INCLUDE_ASM("asm/nonmatchings/Game/MATH3D", MATH3D_LengthXY);
 
 INCLUDE_ASM("asm/nonmatchings/Game/MATH3D", MATH3D_Normalize);
 
-INCLUDE_ASM("asm/nonmatchings/Game/MATH3D", MATH3D_FastAtan2);
+short MATH3D_FastAtan2(long y, long x)
+{
+	long ax;
+	long ay;
+
+	if (x == 0)
+	{
+		x = 1;
+	}
+
+	if (y == 0)
+	{
+		return (x < 1) * 2048;
+	}
+
+	ax = abs(-x);
+	ay = abs(-y);
+
+	if (x > 0)
+	{
+		if (y > 0)
+		{
+			if (ax < ay)
+			{
+				return (short)(1024 - ((ax * 512) / ay));
+			}
+			else
+			{
+				return (short)((ay * 512) / ax);
+			}
+		}
+		else
+		{
+			if (ay < ax)
+			{
+				return (short)(4096 - ((ay * 512) / ax));
+			}
+			else
+			{
+				return (short)(((ax * 512) / ay) + 3072);
+			}
+		}
+	}
+
+	if (y > 0)
+	{
+		if (ax < ay)
+		{
+			return (short)(((ax * 512) / ay) + 1024);
+		}
+		else
+		{
+			return (short)(2048 - ((ay * 512) / ax));
+		}
+	}
+
+	if (ay < ax)
+	{
+		return (short)(((ay * 512) / ax) + 2048);
+	}
+	else
+	{
+		return (short)(3072 - ((ax * 512) / ay));
+	}
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/MATH3D", MATH3D_FastSqrt);
 
