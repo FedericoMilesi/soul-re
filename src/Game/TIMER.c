acaf61490bc6351e9feb368eb79b31e8f0ba78a3
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-05-17 21:41:18 -0300

    Match TIMER_GetTimeMS

--- src/Game/TIMER.c
+++ src/Game/TIMER.c
@@ -1,5 +1,21 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/Game/TIMER", TIMER_GetTimeMS);
+EXTERN volatile int gameTimer;
+
+unsigned long TIMER_GetTimeMS()
+{
+    unsigned long ticks;
+    unsigned long mticks;
+
+    EnterCriticalSection();
+
+    ticks = GetRCnt(0xF2000000) & 0xFFFF;
+
+    mticks = gameTimer;
+
+    ExitCriticalSection();
+
+    return ((mticks >> 16) * 126819) + ((ticks | mticks << 16) / 33869);
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/TIMER", TIMER_TimeDiff);
