3201554a81cbbff6d7e9158933c0cdcf5039d198
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-05-19 16:31:53 -0300

    Match PIPE3D_AspectAdjustMatrix

--- src/Game/PIPE3D.c
+++ src/Game/PIPE3D.c
@@ -1,6 +1,22 @@
 #include "common.h"
+#include "Game/TYPES.h"
 
-INCLUDE_ASM("asm/nonmatchings/Game/PIPE3D", PIPE3D_AspectAdjustMatrix);
+void PIPE3D_AspectAdjustMatrix(MATRIX *matrix)
+{
+    int temp, temp2, temp3; // not from decls.h
+
+    temp = matrix->m[1][0];
+    temp2 = matrix->m[1][1];
+    temp3 = matrix->m[1][2];
+
+    matrix->m[1][0] = temp;
+    matrix->m[1][1] = temp2;
+    matrix->m[1][2] = temp3;
+
+    matrix->m[0][0] = (matrix->m[0][0] * 512) / 320;
+    matrix->m[0][1] = (matrix->m[0][1] * 512) / 320;
+    matrix->m[0][2] = (matrix->m[0][2] * 512) / 320;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/PIPE3D", PIPE3D_CalculateWCTransform);
 
