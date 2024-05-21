de09f356527046f8858c919f8b5e38109d907a62
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-05-21 19:45:45 -0300

    Match MainG2_InitEngine

---
+++ include/Game/G2/MAING2.h
@@ -0,0 +1,8 @@
+#ifndef _MAING2_H_
+#define _MAING2_H_
+
+typedef enum _G2Bool_Enum {
+	G2FALSE = 0,
+} G2Bool;
+
+#endif
--- src/Game/G2/MAING2.c
+++ src/Game/G2/MAING2.c
@@ -1,6 +1,10 @@
 #include "common.h"
+#include "Game/G2/MAING2.h"
 
-INCLUDE_ASM("asm/nonmatchings/Game/G2/MAING2", MainG2_InitEngine);
+G2Bool MainG2_InitEngine(void *appData, unsigned int screenWidth, unsigned int screenHeight, char *filesystemName)
+{
+    return !G2FALSE;
+}
 
 void MainG2_ShutDownEngine(void)
 {
