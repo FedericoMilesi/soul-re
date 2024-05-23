8147ebc5842f16ee2ddf49fd0c89a28735ff3f60
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-05-23 19:15:05 -0300

    Match STREAM_InitLoader

--- src/Game/STRMLOAD.c
+++ src/Game/STRMLOAD.c
@@ -7,6 +7,12 @@ EXTERN STATIC int numLoads;
 
 EXTERN STATIC int loadFromHead;
 
+EXTERN STATIC LoadQueueEntry *loadFree;
+
+EXTERN STATIC LoadQueueEntry *loadHead;
+
+EXTERN STATIC LoadQueueEntry *loadTail;
+
 void STREAM_NextLoadFromHead()
 {
     loadFromHead = 1;
@@ -17,7 +23,27 @@ void STREAM_NextLoadAsNormal()
     loadFromHead = 0;
 }
 
-INCLUDE_ASM("asm/nonmatchings/Game/STRMLOAD", STREAM_InitLoader);
+void STREAM_InitLoader(char *bigFileName, char *voiceFileName)
+{
+    int i;
+
+    LOAD_InitCdLoader(bigFileName, voiceFileName);
+
+    loadFree = (LoadQueueEntry *)&LoadQueue->next;
+
+    loadHead = NULL;
+
+    loadTail = NULL;
+
+    numLoads = 0;
+
+    for (i = 38; i >= 0; i--)
+    {
+        LoadQueue[i].next = &LoadQueue[i + 1];
+    }
+
+    LoadQueue[39].next = NULL;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/STRMLOAD", STREAM_RemoveQueueHead);
 
