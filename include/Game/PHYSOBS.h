e6ee76deb5fa21b738ac95413046c0b27d08a6f6
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-05-25 17:58:28 -0300

    Match WALBOSC_Collide

--- include/Game/PHYSOBS.h
+++ include/Game/PHYSOBS.h
@@ -3,6 +3,60 @@
 
 #include "Game/INSTANCE.h"
 
+// size: 0x8
+typedef struct PhysObSplinter {
+    // offset: 0000
+    long numSplintData;
+    // offset: 0004
+    void *splinterData;
+} PhysObSplinter;
+
+// size: 0x8
+typedef struct PhysObProperties {
+    // offset: 0000
+    unsigned short version;
+    // offset: 0002
+    unsigned short family;
+    // offset: 0004
+    unsigned short ID;
+    // offset: 0006
+    unsigned short Type;
+} PhysObProperties;
+
+// size: 0x1C
+typedef struct PhysObWeaponAttributes {
+    // offset: 0000
+    long Damage;
+    // offset: 0004
+    long AltDamage;
+    // offset: 0008
+    short knockBackDistance;
+    // offset: 000A
+    char knockBackFrames;
+    // offset: 000B
+    char dropSound;
+    // offset: 000C
+    char Class;
+    // offset: 000D
+    char ThrowSphere;
+    // offset: 000E
+    char LeftHandSphere;
+    // offset: 000F
+    char RightHandSphere;
+    // offset: 0010
+    struct _PhysObLight *Light;
+    // offset: 0014 (8 bytes)
+    struct PhysObSplinter splinter;
+} PhysObWeaponAttributes;
+
+// size: 0x24
+typedef struct PhysObWeaponProperties {
+    // offset: 0000 (8 bytes)
+    struct PhysObProperties Properties;
+    // offset: 0008 (28 bytes)
+    struct PhysObWeaponAttributes WeaponAttributes;
+} PhysObWeaponProperties;
+
 // size: 0x10
 typedef struct evObjectBirthProjectileData {
     // offset: 0000 (668 bytes)
--- src/Game/MONSTER/MBMISS.c
+++ src/Game/MONSTER/MBMISS.c
@@ -1,5 +1,8 @@
 #include "common.h"
 #include "Game/INSTANCE.h"
+#include "Game/GAMELOOP.h"
+#include "Game/COLLIDE.h"
+#include "Game/PHYSOBS.h"
 #include "Game/G2/ANIMG2.h"
 
 INCLUDE_ASM("asm/nonmatchings/Game/MONSTER/MBMISS", WCBEGG_Message);
@@ -40,7 +43,30 @@ long WALBOSC_AnimCallback(G2Anim *anim, int sectionID, G2AnimCallbackMsg message
     return messageDataA;
 }
 
-INCLUDE_ASM("asm/nonmatchings/Game/MONSTER/MBMISS", WALBOSC_Collide);
+void WALBOSC_Collide(Instance *instance, GameTracker *gameTracker)
+{
+    CollideInfo *collideInfo;
+    HSphere *s1;
+    Instance *inst1;
+
+    collideInfo = (CollideInfo *)instance->collideInfo;
+
+    inst1 = collideInfo->inst1;
+
+    s1 = collideInfo->prim1;
+
+    if (s1->id == 8)
+    {
+        PhysObWeaponProperties *Prop;
+
+        Prop = (PhysObWeaponProperties *)instance->data;
+
+        COLLIDE_SegmentCollisionOff(instance, 1);
+
+        INSTANCE_Post(inst1, 0x400000, SetFXHitData(instance, (unsigned char)collideInfo->segment, Prop->WeaponAttributes.Damage / 128, 256));
+        INSTANCE_Post(inst1, 0x1000000, SetMonsterHitData(instance, NULL, Prop->WeaponAttributes.Damage, Prop->WeaponAttributes.knockBackDistance, Prop->WeaponAttributes.knockBackFrames));
+    }
+}
 
 void WALBOSC_Message(Instance *instance, unsigned long message, unsigned long data)
 {
