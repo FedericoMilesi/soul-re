110ff395aedd55487d842bfbcce4ae6bdd65bce4
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-05-29 16:45:41 -0300

    Fix warning

---
+++ include/Game/G2/ANMG2ILF.h
@@ -0,0 +1,9 @@
+#ifndef _ANMG2ILF_H_
+#define _ANMG2ILF_H_
+
+#include "common.h"
+#include "Game/G2/ANIMG2.h"
+
+void G2Anim_SwitchToKeylist(G2Anim *anim, G2AnimKeylist *keylist, int keylistID);
+
+#endif
