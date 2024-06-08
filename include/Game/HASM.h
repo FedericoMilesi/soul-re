06f6d973a8a123582a8664cae4c15da9bfbdf7eb
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-06-07 22:27:25 -0300

    Match G2Quat_ToEuler

--- include/Game/HASM.h
+++ include/Game/HASM.h
@@ -2,10 +2,12 @@
 #define _HASM_H_
 
 #include "common.h"
+#include "Game/G2/QUATG2.h"
 
 int rcos(int);
 int rsin(int);
 void RotMatrix(SVECTOR *, MATRIX *);
 void ScaleMatrix(MATRIX *, VECTOR *);
+void G2Quat_ToMatrix_S(G2Quat *quat, G2Matrix *matrix);
 
 #endif
--- src/Game/G2/QUATG2.c
+++ src/Game/G2/QUATG2.c
@@ -1,5 +1,14 @@
 #include "common.h"
+#include "Game/G2/QUATG2.h"
+#include "Game/HASM.h"
 
-INCLUDE_ASM("asm/nonmatchings/Game/G2/QUATG2", G2Quat_ToEuler);
+void G2Quat_ToEuler(G2Quat *quat, G2EulerAngles *euler, int order)
+{
+    G2Matrix tempMatrix;
+
+    G2Quat_ToMatrix_S(quat, &tempMatrix);
+
+    G2EulerAngles_FromMatrix(euler, &tempMatrix, order);
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/G2/QUATG2", G2EulerAngles_FromMatrix);
