36aa8aa517f4090d414e2df0116eab0ae891515d
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-06-07 22:33:04 -0300

    Match G2PoolMem_Free

--- src/Game/G2/POOLMMG2.c
+++ src/Game/G2/POOLMMG2.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "Game/G2/POOLMMG2.h"
 
 INCLUDE_ASM("asm/nonmatchings/Game/G2/POOLMMG2", G2PoolMem_InitPool);
 
@@ -6,4 +7,11 @@ INCLUDE_ASM("asm/nonmatchings/Game/G2/POOLMMG2", G2PoolMem_ResetPool);
 
 INCLUDE_ASM("asm/nonmatchings/Game/G2/POOLMMG2", G2PoolMem_Allocate);
 
-INCLUDE_ASM("asm/nonmatchings/Game/G2/POOLMMG2", G2PoolMem_Free);
+void G2PoolMem_Free(void *voidPool, void *block)
+{
+    int blockIndex;
+
+    blockIndex = (((char *)block) - ((char *)((G2PoolMemPool *)voidPool)->blockPool)) / ((G2PoolMemPool *)voidPool)->blockSize;
+
+    ((G2PoolMemPool *)voidPool)->stack[--((G2PoolMemPool *)voidPool)->stackTop] = blockIndex;
+}
