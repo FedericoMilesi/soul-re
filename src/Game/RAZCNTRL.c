1c792e4b6605569d0ef964fdf971042a4ca7a581
Gillou68310 <gilles.siberlin@outlook.com>
2024-06-10 12:43:55 +0200

    Match ProcessRazControl + Migrate RAZCNTRL.c sdata to C

--- src/Game/RAZCNTRL.c
+++ src/Game/RAZCNTRL.c
@@ -1,3 +1,40 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/Game/RAZCNTRL", ProcessRazControl);
+long RazielCommands[10] = {0x8000, 0x40, 0x20, 0x80, 0x400, 0x800, 0x100, 0x200, 0xC00, 0x10};
+
+static int Pending = 0;
+
+static int Up = 0;
+
+static int Down = 0;
+
+void ProcessRazControl(long *command)
+{
+    if ((*command & RazielCommands[2]))
+    {
+        Up = 0;
+
+        Down++;
+    }
+    else
+    {
+        Up++;
+
+        if (Down != 0)
+        {
+            Pending = Down;
+        }
+
+        Down = 0;
+    }
+
+    if (Down >= 6)
+    {
+        Pending = Down;
+    }
+
+    if (Up >= 3)
+    {
+        Pending = 0;
+    }
+}
