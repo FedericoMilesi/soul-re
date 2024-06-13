4ba18ca21c58dc3ef0918c4732ae66c281b7090b
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-06-13 03:03:03 -0300

    Match DeInitAlgorithmicWings

--- include/Game/G2/ANMCTRLR.h
+++ include/Game/G2/ANMCTRLR.h
@@ -4,5 +4,6 @@
 #include "Game/G2/ANIMG2.h"
 
 G2Bool G2Anim_IsControllerActive(G2Anim *anim, int segNumber, int type);
+void G2Anim_DisableController(G2Anim *anim, int segNumber, int type);
 
 #endif
--- src/Game/RAZIEL/ALGOCTRL.c
+++ src/Game/RAZIEL/ALGOCTRL.c
@@ -1,8 +1,28 @@
 #include "common.h"
 
+EXTERN STATIC int AlgoControlFlag;
+
 INCLUDE_ASM("asm/nonmatchings/Game/RAZIEL/ALGOCTRL", InitAlgorithmicWings);
 
-INCLUDE_ASM("asm/nonmatchings/Game/RAZIEL/ALGOCTRL", DeInitAlgorithmicWings);
+void DeInitAlgorithmicWings(Instance *instance)
+{
+    unsigned char i;
+
+    if ((AlgoControlFlag & 0x1))
+    {
+        for (i = 51; i < 54; i++)
+        {
+            G2Anim_DisableController(&instance->anim, i, 8);
+        }
+
+        for (i = 59; i < 62; i++)
+        {
+            G2Anim_DisableController(&instance->anim, i, 8);
+        }
+
+        AlgoControlFlag &= ~0x1;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/RAZIEL/ALGOCTRL", AlgorithmicWings);
 
