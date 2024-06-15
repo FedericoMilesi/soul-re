01f65e0bc0c6a2af743a4fa57c27775aac01b9de
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-06-15 04:05:51 -0300

    Match _SplineS2Pos

--- src/Game/SPLINE.c
+++ src/Game/SPLINE.c
@@ -1,6 +1,45 @@
 #include "common.h"
+#include "Game/SPLINE.h"
 
-INCLUDE_ASM("asm/nonmatchings/Game/SPLINE", _SplineS2Pos);
+void _SplineS2Pos(vecS *p, long s, SplineKey *key, SplineKey *key2)
+{
+    long s2;
+    long s3;
+    long h0;
+    long h1;
+    long h2;
+    long h3;
+    int temp, temp2; // not from decls.h
+
+    if (s != 0)
+    {
+        s2 = (s * s) >> 15;
+
+        temp = s2 * 2;
+
+        s3 = (s2 * s) >> 15;
+
+        temp2 = s3 * 2;
+
+        h0 = (temp2 - (s2 * 3)) + 32768;
+
+        h1 = (temp - temp2) + s2;
+
+        h2 = (s3 - temp) + s;
+
+        h3 = s3 - s2;
+
+        p->x = (short)(((key->point.x * h0) + (key2->point.x * h1) + (key->dd.x * h2) + (key2->ds.x * h3)) >> 15);
+        p->y = (short)(((key->point.y * h0) + (key2->point.y * h1) + (key->dd.y * h2) + (key2->ds.y * h3)) >> 15);
+        p->z = (short)(((key->point.z * h0) + (key2->point.z * h1) + (key->dd.z * h2) + (key2->ds.z * h3)) >> 15);
+    }
+    else
+    {
+        p->x = key->point.x;
+        p->y = key->point.y;
+        p->z = key->point.z;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/SPLINE", SplineSetDefDenom);
 
