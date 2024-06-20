5c3d054f3b67bbdd5094fd03951bd95430fd0e22
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-06-20 11:40:05 -0300

    Add header file

---
+++ include/Game/LOCAL/LOCALSTR.h
@@ -0,0 +1,14 @@
+#ifndef _LOCALSTR_H_
+#define _LOCALSTR_H_
+
+typedef enum language_t {
+	language_english    = 0,
+	language_french     = 1,
+	language_german     = 2,
+	language_italian    = 3,
+	language_spanish    = 4,
+	language_num_states = 5,
+	language_default    = 4294967295,
+} language_t;
+
+#endif
