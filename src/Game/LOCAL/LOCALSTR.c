2d57670745ed84e8b10010645846e8ef9651caae
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-06-24 12:28:54 -0300

    Match localstr_get_language

--- src/Game/LOCAL/LOCALSTR.c
+++ src/Game/LOCAL/LOCALSTR.c
@@ -1,6 +1,12 @@
 #include "common.h"
+#include "Game/LOCAL/LOCALSTR.h"
 
-INCLUDE_ASM("asm/nonmatchings/Game/LOCAL/LOCALSTR", localstr_get_language);
+language_t the_language;
+
+language_t localstr_get_language()
+{
+    return the_language;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/LOCAL/LOCALSTR", localstr_set_language);
 
