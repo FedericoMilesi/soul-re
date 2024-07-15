76f637f50e426f6e98d6caf20d6900a381a158db
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-07-15 14:34:34 -0300

    Match _G2Anim_DecompressChannel_Linear

--- src/Game/G2/ANMDECMP.c
+++ src/Game/G2/ANMDECMP.c
@@ -3,7 +3,50 @@
 
 INCLUDE_ASM("asm/nonmatchings/Game/G2/ANMDECMP", _G2Anim_DecompressChannel_AdaptiveDelta);
 
-INCLUDE_ASM("asm/nonmatchings/Game/G2/ANMDECMP", _G2Anim_DecompressChannel_Linear);
+void _G2Anim_DecompressChannel_Linear(G2AnimDecompressChannelInfo *dcInfo, G2AnimChanStatus *status)
+{
+    unsigned short *chanData;
+    short rangeBase;
+    short rangeInfo;
+    int rangeLength;
+    int rangeOffset;
+    int targetKey;
+
+    chanData = dcInfo->chanData;
+
+    targetKey = dcInfo->targetKey;
+
+    dcInfo->chanData = &chanData[((chanData[0] & 0xFFF)) + 1];
+
+    rangeBase = chanData[1];
+
+    rangeInfo = chanData[2]; // double-check
+
+    chanData++;
+
+    rangeLength = (rangeInfo & 0xF800) >> 11;
+
+    rangeOffset = (rangeInfo << 21) >> 21;
+
+    while (rangeLength < targetKey)
+    {
+        rangeInfo = chanData[2]; // double-check
+
+        targetKey -= rangeLength;
+
+        rangeBase += rangeOffset;
+
+        chanData++;
+
+        rangeLength = (rangeInfo & 0xF800) >> 11;
+
+        rangeOffset = (rangeInfo << 21) >> 21;
+    }
+
+    rangeOffset = (short)((rangeOffset * targetKey) / rangeLength);
+
+    status->keyData = rangeBase + rangeOffset;
+}
 
 void _G2Anim_InitializeChannel_AdaptiveDelta(G2AnimDecompressChannelInfo *dcInfo, G2AnimChanStatus *status)
 {
