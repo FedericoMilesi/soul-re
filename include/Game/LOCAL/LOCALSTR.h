31605457b25b57d6c00fbd80004c93b26b8e3a8b
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-07-17 16:16:26 -0300

    Fix warning

--- include/Game/LOCAL/LOCALSTR.h
+++ include/Game/LOCAL/LOCALSTR.h
@@ -2,13 +2,14 @@
 #define _LOCALSTR_H_
 
 typedef enum language_t {
-	language_english    = 0,
-	language_french     = 1,
-	language_german     = 2,
-	language_italian    = 3,
-	language_spanish    = 4,
-	language_num_states = 5,
-	language_default    = 4294967295,
+    language_english = 0,
+    language_french = 1,
+    language_german = 2,
+    language_italian = 3,
+    language_spanish = 4,
+    language_num_states = 5,
+    //language_default    = 4294967295,
+    language_default = 0xFFFFFFFF,
 } language_t;
 
 #endif
