d1737da8841d20d488db80e3209259bc90b748c2
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-07-17 13:03:39 -0300

    Match SetImpulsePhysics

--- src/Game/RAZIEL/CONTROL.c
+++ src/Game/RAZIEL/CONTROL.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "Game/PLAYER.h"
 
 INCLUDE_ASM("asm/nonmatchings/Game/RAZIEL/CONTROL", SetPhysics);
 
@@ -6,7 +7,29 @@ INCLUDE_ASM("asm/nonmatchings/Game/RAZIEL/CONTROL", ResetPhysics);
 
 INCLUDE_ASM("asm/nonmatchings/Game/RAZIEL/CONTROL", SetDampingPhysics);
 
-INCLUDE_ASM("asm/nonmatchings/Game/RAZIEL/CONTROL", SetImpulsePhysics);
+void SetImpulsePhysics(Instance *instance, Player *player)
+{
+    int Dot;
+    int vLength;
+
+    if (!(player->Mode & 0x40000))
+    {
+        Dot = -instance->offset.x * player->iVelocity.x;
+        Dot += -instance->offset.y * player->iVelocity.y;
+        Dot += -instance->offset.z * player->iVelocity.z;
+
+        vLength = player->iVelocity.x * player->iVelocity.x;
+        vLength += player->iVelocity.y * player->iVelocity.y;
+        vLength += player->iVelocity.z * player->iVelocity.z;
+
+        if (vLength != 0)
+        {
+            instance->position.x += instance->offset.x + ((Dot * player->iVelocity.x) / vLength);
+            instance->position.y += instance->offset.y + ((Dot * player->iVelocity.y) / vLength);
+            instance->position.z += instance->offset.z + ((Dot * player->iVelocity.z) / vLength);
+        }
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/RAZIEL/CONTROL", SetDropPhysics);
 
