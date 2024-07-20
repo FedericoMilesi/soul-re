89523c05997a7d6075e6943d5b9586b88758fd58
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-07-20 18:33:45 -0300

    Match mytolower

--- src/Game/PSX/SUPPORT.c
+++ src/Game/PSX/SUPPORT.c
@@ -10,6 +10,14 @@ INCLUDE_ASM("asm/nonmatchings/Game/PSX/SUPPORT", my_itoa);
 
 INCLUDE_ASM("asm/nonmatchings/Game/PSX/SUPPORT", atoi);
 
-INCLUDE_ASM("asm/nonmatchings/Game/PSX/SUPPORT", mytolower);
+int mytolower(int c)
+{
+    if ((c >= 'A') && (c <= 'Z'))
+    {
+        c += 32;
+    }
+
+    return c;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/PSX/SUPPORT", strcmpi);
