37383c5384f403256838903795c30ee8bd697532
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-07-24 14:02:39 -0300

    Create MEMCARD and MENU headers

---
+++ include/Game/MCARD/MEMCARD.h
@@ -0,0 +1,10 @@
+#ifndef _MEMCARD_H_
+#define _MEMCARD_H_
+
+#include "common.h"
+
+typedef struct memcard_t memcard_t;
+
+void memcard_end(memcard_t *memcard);
+
+#endif
---
+++ include/Game/MENU/MENU.h
@@ -0,0 +1,10 @@
+#ifndef _MENU_H_
+#define _MENU_H_
+
+#include "common.h"
+
+typedef struct menu_t menu_t;
+
+void menu_pop(menu_t *menu);
+
+#endif
