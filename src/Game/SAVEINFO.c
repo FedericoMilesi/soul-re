14e650b967fa958e5816543e51b29d8d4d94a95d
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-08-05 17:35:38 -0300

    Tidy up SAVE_DebugSaveGame

--- src/Game/SAVEINFO.c
+++ src/Game/SAVEINFO.c
@@ -70,7 +70,7 @@ INCLUDE_ASM("asm/nonmatchings/Game/SAVEINFO", SAVE_SaveGame);
 
 INCLUDE_ASM("asm/nonmatchings/Game/SAVEINFO", SAVE_RestoreGame);
 
-void SAVE_DebugSaveGame(void)
+void SAVE_DebugSaveGame()
 {
 }
 
