ffe4e6265144f3cb520069d30e76c5db9adb3ead
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-09-23 11:44:10 -0300

    Match UpdateAutoFaceAngle

--- src/Game/RAZIEL/SENSES.c
+++ src/Game/RAZIEL/SENSES.c
@@ -1,7 +1,15 @@
 #include "common.h"
+#include "Game/MATH3D.h"
 
 INCLUDE_ASM("asm/nonmatchings/Game/RAZIEL/SENSES", SetEngagedInstance);
 
 INCLUDE_ASM("asm/nonmatchings/Game/RAZIEL/SENSES", UpdateEngagementList);
 
-INCLUDE_ASM("asm/nonmatchings/Game/RAZIEL/SENSES", UpdateAutoFaceAngle);
+long UpdateAutoFaceAngle(Instance *player, Instance *instance)
+{
+    long rc;
+
+    rc = MATH3D_FastAtan2(player->position.y - instance->position.y, player->position.x - instance->position.x);
+
+    return (rc + 3072) & 0xFFF;
+}
