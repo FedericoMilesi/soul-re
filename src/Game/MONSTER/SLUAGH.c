91de6d3472e4a54d5e4bc3b79506f40dac186eb6
Rhyse Thompson <rhysethompson@gmail.com>
2024-09-24 14:19:36 -0500

    Match SLUAGH_DeathEntry

--- src/Game/MONSTER/SLUAGH.c
+++ src/Game/MONSTER/SLUAGH.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "Game/FX.h"
 #include "Game/MONSTER/MONAPI.h"
+#include "Game/MONSTER/MONLIB.h"
 #include "Game/MONSTER/MONSTER.h"
 
 INCLUDE_ASM("asm/nonmatchings/Game/MONSTER/SLUAGH", SLUAGH_Query);
@@ -30,7 +31,41 @@ void SLUAGH_Init(Instance *instance)
 
 }
 
-INCLUDE_ASM("asm/nonmatchings/Game/MONSTER/SLUAGH", SLUAGH_DeathEntry);
+void SLUAGH_DeathEntry(Instance *instance)
+{
+    
+    MonsterVars *mv;
+    MonsterIR *enemy;
+
+    mv = (MonsterVars*) instance->extraData;
+    FX_StopGlowEffect(mv->effect, 0);
+    
+    enemy = mv->enemy;
+    mv->effect = 0;
+        
+    if (enemy != NULL)
+    {
+        if (MON_SetUpKnockBack(instance, enemy->instance, (evMonsterHitData*) mv->messageData) != 0)
+        {
+            MON_PlayAnim(instance, MONSTER_ANIM_HIT1, 1);
+        }
+        else
+        {
+            MON_PlayAnim(instance, MONSTER_ANIM_HIT2, 1); 
+        }
+    }
+    
+    mv->behaviorState = MONSTER_STATE_STUNNED;
+    
+    if (!(mv->auxFlags & 1))
+    {
+        MON_BirthMana(instance);
+        mv->auxFlags |= 1;
+    }
+    
+    mv->mvFlags |= 0x200000;
+    MON_StartSpecialFade(instance, 0x800, 0x14);
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/MONSTER/SLUAGH", SLUAGH_Death);
 
