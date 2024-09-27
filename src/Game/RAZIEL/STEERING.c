cca53b1ba30d0d5801e56d2873c681b2816a76c2
Federico <fmilesi_soft@hotmail.com>
2024-09-27 19:51:39 -0300

    Add non-matching SteerSwitchMode
    Co-Authored-By: Mark Street <22226349+mkst@users.noreply.github.com>
    Co-Authored-By: Unnunu <20560532+unnunu@users.noreply.github.com>
    Co-Authored-By: Wiseguy <68165316+mr-wiseguy@users.noreply.github.com>

--- src/Game/RAZIEL/STEERING.c
+++ src/Game/RAZIEL/STEERING.c
@@ -97,7 +97,131 @@ void SteerDisableAutoFace(Instance *instance)
     Raziel.autoFaceAnim = -1;
 }
 
+// Matches 100% on decomp.me but differs on this project
+#ifndef NON_MATCHING
 INCLUDE_ASM("asm/nonmatchings/Game/RAZIEL/STEERING", SteerSwitchMode);
+#else
+void SteerSwitchMode(Instance *instance, int mode)
+{
+    switch (Raziel.steeringMode)
+    {
+    case 0:
+    case 1:
+    case 2:
+    case 4:
+    case 8:
+    case 18:
+        if (G2Anim_IsControllerActive(&instance->anim, 1, 14) != G2FALSE)
+        {
+            G2Anim_DisableController(&instance->anim, 1, 14);
+
+            instance->rotation.z = Raziel.steeringLockRotation;
+
+            Raziel.LastBearing = Raziel.steeringLockRotation;
+        }
+
+        break;
+    case 5:
+    case 9:
+    case 15:
+        SteerDisableAutoFace(instance);
+        break;
+    case 6:
+    case 17:
+        if ((mode != 6) && (mode != 11) && (mode != 16) && (mode != 17))
+        {
+            if (G2Anim_IsControllerActive(&instance->anim, 1, 14) != G2FALSE)
+            {
+                G2Anim_InterpDisableController(&instance->anim, 1, 14, 600);
+            }
+
+            Raziel.extraRot.x = 0;
+        }
+
+        break;
+    case 7:
+        if (mode != Raziel.steeringMode)
+        {
+            razDeinitWallCrawlSteering(instance);
+        }
+
+        break;
+    case 10:
+        Raziel.attackedBy = NULL;
+        break;
+    case 11:
+        CAMERA_EndSwimThrowMode(&theCamera);
+
+        CAMERA_SetLookRot(&theCamera, 0, 0);
+
+        Raziel.extraRot.x = Raziel.throwReturnRot;
+        break;
+    }
+
+    Raziel.RotationSegment = 0;
+
+    switch (mode)
+    {
+    case 0:
+        Raziel.extraRot.z = 0;
+        Raziel.extraRot.x = 0;
+
+        Raziel.RotationSegment = 0;
+        break;
+    case 1:
+    case 2:
+    case 4:
+    case 5:
+    case 8:
+    case 9:
+    case 10:
+    case 15:
+    case 18:
+        Raziel.RotationSegment = 0;
+        break;
+    case 11:
+    {
+        int rotx;
+
+        Raziel.throwReturnRot = Raziel.extraRot.x;
+
+        rotx = 4096 - theCamera.core.rotation.x;
+        rotx &= 0xFFF;
+
+        if ((rotx > 1024) && (rotx < 2048))
+        {
+            rotx = 1024;
+        }
+        else if ((rotx > 2048) && (rotx < 3072))
+        {
+            rotx = 3072;
+        }
+
+        Raziel.extraRot.x = rotx;
+
+        CAMERA_StartSwimThrowMode(&theCamera);
+
+        CAMERA_SetLookRot(&theCamera, 4096 - Raziel.extraRot.x, 0);
+    }
+    case 6:
+    case 17:
+        Raziel.RotationSegment = 1;
+        break;
+    case 7:
+        if (mode != Raziel.steeringMode)
+        {
+            razInitWallCrawlSteering(instance);
+        }
+
+        break;
+    case 14:
+        instance->rotation.z = MATH3D_AngleFromPosToPos(&instance->position, &Raziel.Senses.EngagedList[6].instance->position);
+        break;
+    }
+
+    Raziel.steeringMode = mode;
+}
+#endif
 
 void razInitWallCrawlSteering(Instance *instance)
 {
