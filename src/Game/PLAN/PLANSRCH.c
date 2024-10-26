140e73eb765ee10786624d3d0abe0fcd37793922
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-10-26 01:40:01 -0300

    Match PLANSRCH_InitNodesForSearch

--- src/Game/PLAN/PLANSRCH.c
+++ src/Game/PLAN/PLANSRCH.c
@@ -1,9 +1,23 @@
 #include "common.h"
+#include "Game/PLAN/PLAN.h"
+#include "Game/PLAN/PLANAPI.h"
 
 INCLUDE_ASM("asm/nonmatchings/Game/PLAN/PLANSRCH", PLANSRCH_FindNodeToExpand);
 
 INCLUDE_ASM("asm/nonmatchings/Game/PLAN/PLANSRCH", PLANSRCH_ExpandNode);
 
-INCLUDE_ASM("asm/nonmatchings/Game/PLAN/PLANSRCH", PLANSRCH_InitNodesForSearch);
+void PLANSRCH_InitNodesForSearch(PlanningNode *planningPool)
+{
+    int i;
+
+    for (i = 0; i < poolManagementData->numNodesInPool; i++)
+    {
+        planningPool[i].cost = 0;
+
+        planningPool[i].parent = 0;
+
+        planningPool[i].flags &= ~0x3;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/PLAN/PLANSRCH", PLANSRCH_FindPathInGraph);
