9f41a1bdaf6169704035038230079e8aa84eb821
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-10-27 19:13:49 -0300

    Match PLANCOLL_DoesWaterPathUpExist

--- include/Game/PLAN/PLANCOLL.h
+++ include/Game/PLAN/PLANCOLL.h
@@ -4,5 +4,6 @@
 #include "common.h"
 
 int PLANCOLL_DoesStraightLinePathExist(Position *startPos, Position *endPos, int collideType);
+int PLANCOLL_DoesLOSExistFinal(Position *startPos, Position *endPos, int collideType, int passThroughHit, int zoffset);
 
 #endif
--- src/Game/PLAN/PLANCOLL.c
+++ src/Game/PLAN/PLANCOLL.c
@@ -1,4 +1,6 @@
 #include "common.h"
+#include "Game/PLAN/PLANCOLL.h"
+#include "Game/MATH3D.h"
 
 INCLUDE_ASM("asm/nonmatchings/Game/PLAN/PLANCOLL", PLANCOLL_DoesLOSExistFinal);
 
@@ -8,4 +10,26 @@ INCLUDE_ASM("asm/nonmatchings/Game/PLAN/PLANCOLL", PLANCOLL_FindTerrainHitFinal)
 
 INCLUDE_ASM("asm/nonmatchings/Game/PLAN/PLANCOLL", PLANCOLL_DoesStraightLinePathExist);
 
-INCLUDE_ASM("asm/nonmatchings/Game/PLAN/PLANCOLL", PLANCOLL_DoesWaterPathUpExist);
+int PLANCOLL_DoesWaterPathUpExist(Position *startPos, Position *endPos, int collideType, Position *peakPos, int passThroughHit)
+{
+    long time_ftop;
+    long time_ptow;
+    long time_tot;
+    Position diff;
+
+    time_ftop = MATH3D_FastSqrt(327680);
+
+    peakPos->z = (startPos->z - endPos->z) + 640;
+
+    time_ptow = MATH3D_FastSqrt((peakPos->z * 131072) / 3);
+
+    time_tot = time_ptow / 16;
+
+    diff.x = startPos->x - endPos->x;
+    diff.y = startPos->y - endPos->y;
+
+    peakPos->x = endPos->x + ((diff.x * time_tot) / (time_ftop + time_tot));
+    peakPos->y = endPos->y + ((diff.y * time_tot) / (time_ftop + time_tot));
+
+    return PLANCOLL_DoesLOSExistFinal(startPos, peakPos, collideType, passThroughHit, 0);
+}
