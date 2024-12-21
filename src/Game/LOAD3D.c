2d876c06018b1d4d82bbb9d7b3b33138d9f90435
FedericoMilesi <fmilesi_soft@hotmail.com>
2024-12-21 16:13:10 -0300

    Match LOAD_StopLoad

--- src/Game/LOAD3D.c
+++ src/Game/LOAD3D.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+STATIC LoadStatus loadStatus;
+
 INCLUDE_ASM("asm/nonmatchings/Game/LOAD3D", LOAD_InitCd);
 
 INCLUDE_ASM("asm/nonmatchings/Game/LOAD3D", LOAD_CdSeekCallback);
@@ -70,4 +72,14 @@ INCLUDE_ASM("asm/nonmatchings/Game/LOAD3D", LOAD_UpdateBigFilePointers);
 
 INCLUDE_ASM("asm/nonmatchings/Game/LOAD3D", LOAD_IsFileLoading);
 
-INCLUDE_ASM("asm/nonmatchings/Game/LOAD3D", LOAD_StopLoad);
+void LOAD_StopLoad()
+{
+    loadStatus.state = 5;
+
+    loadStatus.currentQueueFile.readStatus = 0;
+
+    if (loadStatus.currentDirLoading != 0)
+    {
+        loadStatus.currentDirLoading = 0;
+    }
+}
