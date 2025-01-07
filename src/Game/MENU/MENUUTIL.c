0571625d150b6271dd8c99f3115677b38c8ca68d
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-01-07 15:49:55 +0000

    Match menu_text_width

--- src/Game/MENU/MENUUTIL.c
+++ src/Game/MENU/MENUUTIL.c
@@ -1,9 +1,13 @@
 #include "Game/MENU/MENUUTIL.h"
+#include "Game/FONT.h"
 
 INCLUDE_ASM("asm/nonmatchings/Game/MENU/MENUUTIL", menu_get_ctrl);
 
 INCLUDE_ASM("asm/nonmatchings/Game/MENU/MENUUTIL", menu_print);
 
-INCLUDE_ASM("asm/nonmatchings/Game/MENU/MENUUTIL", menu_text_width);
+int menu_text_width(char *text)
+{
+    FONT_GetStringWidth(text);
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/MENU/MENUUTIL", menu_sound);
