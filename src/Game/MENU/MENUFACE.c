d2e03858c2b991995d68908ad9b80221573034ec
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-01-07 16:40:32 +0000

    Match NextTimAddr

--- src/Game/MENU/MENUFACE.c
+++ src/Game/MENU/MENUFACE.c
@@ -1,6 +1,26 @@
 #include "Game/MENU/MENUFACE.h"
 
-INCLUDE_ASM("asm/nonmatchings/Game/MENU/MENUFACE", NextTimAddr);
+char *NextTimAddr(char *addr, int w, int h, bdepth bpp)
+{
+    long addtl;
+
+    if (bpp == TIM_4BIT)
+    {
+        addtl = ((w * h) >> 1) + 44;
+    }
+    else if (bpp == TIM_8BIT)
+    {
+        addtl = (w * h) + 524;
+    }
+    else
+    {
+        addtl = (w * h) << 1;
+    }
+
+    addtl += 20;
+
+    return &addr[addtl];
+}
 
 INCLUDE_ASM("asm/nonmatchings/Game/MENU/MENUFACE", menuface_initialize);
 
