9fcce4601e89c424f373013d808e66f7b09197f8
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-01-07 15:54:28 +0000

    Match menu_sound

--- src/Game/MENU/MENUUTIL.c
+++ src/Game/MENU/MENUUTIL.c
@@ -1,5 +1,8 @@
 #include "Game/MENU/MENUUTIL.h"
 #include "Game/FONT.h"
+#include "Game/SOUND.h"
+
+STATIC menu_sound_entry_t the_menu_sounds[8924 + 6];
 
 INCLUDE_ASM("asm/nonmatchings/Game/MENU/MENUUTIL", menu_get_ctrl);
 
@@ -10,4 +13,11 @@ int menu_text_width(char *text)
     FONT_GetStringWidth(text);
 }
 
-INCLUDE_ASM("asm/nonmatchings/Game/MENU/MENUUTIL", menu_sound);
+void menu_sound(menu_sound_t sound)
+{
+    menu_sound_entry_t *entry;
+
+    entry = &the_menu_sounds[sound];
+
+    SndPlay(entry->sfx);
+}
